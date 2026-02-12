a8f2b54281ff954c8faf3859668b3e98186fe606
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2026-02-12 18:11:19 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -40,7 +40,7 @@ exports.login = async (req, res,next) => {
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id,role:user.role }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
-    res.status(200).json({ token, user: { id: user._id,email:user.email, username: user.username, role: user.role } });
+    res.status(200).json({ token, user: { id: user._id,email:user.email, username: user.username} });
   } catch (err) {
     console.error(err);
     next(err)
