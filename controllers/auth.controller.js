b9c075aafb6ef83dd4233215b9cb99e8b88d48b4
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2026-02-18 23:18:49 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -48,9 +48,9 @@ exports.login = async (req, res,next) => {
     res.cookie('jwt', refreshToken, {
             httpOnly: true,
             secure: true,
-           sameSite: 'None',
+            sameSite: 'None',
             maxAge: 15 * 24 * 60 * 60 * 1000,
-            path: '/auth/refresh'
+            path: '/'
         });
     res.status(200).json({ accessToken, user: { id: user._id,email:user.email, username: user.username} });
   } catch (err) {
